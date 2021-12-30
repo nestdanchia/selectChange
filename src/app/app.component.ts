42f71174eff01a8743420e72d0c8f35bff374654
nestdanchia <ndchiariello@gmail.com>
2021-12-30 18:44:52 -0300

    jiji

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,36 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  estado!: string;
-  selectedValue!: string;
-  estados = [
-    {
-      id: 'paid',
-      label: 'Pagado',
-    },
-    {
-      id: 'toPay',
-      label: 'Por pagar',
-    },
 
-  ];
-  constructor() {
-
-  }
-  ngOnInit() {
-    this.selectedValue = localStorage.getItem('estado') || 'default-token';
-  }
-
-  onChange(event: Event) {
-
-    this.estado = (event.target as HTMLInputElement).value;
-    localStorage.setItem('estado', this.estado);
-    this.selectedValue = localStorage.getItem('estado') || 'default-token';
-    console.log('this.selectedValue', this.selectedValue, localStorage.getItem('estado'))
-
-  }
-  removeItemStorage(key: string) {
-    localStorage.removeItem(key);
-  }
 
 }
