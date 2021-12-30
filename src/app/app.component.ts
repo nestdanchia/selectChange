84be2c4be65f4018faebb081edde1599b79c015c
nestdanchia <ndchiariello@gmail.com>
2021-12-30 18:42:10 -0300

    cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
-
+//https://github.com/nestdanchia/selectChange.git
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'tourt';
+  estado!: string;
+  selectedValue!: string;
+  estados = [
+    {
+      id: 'paid',
+      label: 'Pagado',
+    },
+    {
+      id: 'toPay',
+      label: 'Por pagar',
+    },
+
+  ];
+  constructor() {
+
+  }
+  ngOnInit() {
+    this.selectedValue = localStorage.getItem('estado') || 'default-token';
+  }
+
+  onChange(event: Event) {
+
+    this.estado = (event.target as HTMLInputElement).value;
+    localStorage.setItem('estado', this.estado);
+    this.selectedValue = localStorage.getItem('estado') || 'default-token';
+    console.log('this.selectedValue', this.selectedValue, localStorage.getItem('estado'))
+
+  }
+  removeItemStorage(key: string) {
+    localStorage.removeItem(key);
+  }
+
 }
